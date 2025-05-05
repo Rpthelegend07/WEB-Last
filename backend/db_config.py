279a36df4815a2ab3b7a49c7d6d0217d6b0b16ea
Rushi Patel <w0847987@myscc.ca>
2025-05-05 02:31:14 -0400

    fuckign

--- backend/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def connect():
-    return mysql.connector.connect(
-        host="php.scweb.ca",
-        user="rpatel5",
-        password="z66zaz66za",
-        database="rpatel5"
-    )
